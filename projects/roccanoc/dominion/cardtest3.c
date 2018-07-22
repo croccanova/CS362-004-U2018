51aa28163c977b36dc1f5ab5f00b621ac3a10810
Christian Roccanova <roccanoc@flip2.engr.oregonstate.edu>
2018-07-22 15:05:17 -0700

    added files for unit testing

---
+++ projects/roccanoc/dominion/cardtest3.c
@@ -0,0 +1,138 @@
+/*
+	Christian Roccanova
+	CS362-400
+	Assignment 3
+	Tests outpost card
+*/
+
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <stdio.h>
+#include "rngs.h"
+
+// counter for total number of failures
+int failCount = 0;
+
+// checks if two values are equivalent
+void isTrue(int a, int b)
+{
+	if (a == b)
+	{
+		printf("TEST PASSED\n");
+	}
+	else
+	{
+		printf("TEST FAILED\n");
+		failCount++;
+	}
+}
+
+int main(int argc, char** argv)
+{
+	//set arguments for initializeGame and cardEffect
+	struct gameState control, test;
+	int numPlayers = 2;
+	int handPos = 0, choice1 = -1, choice2 = -1, choice3 = -1, bonus = 0;
+	int seed = 1000;
+	int k[] = { adventurer, council_room, feast, gardens, mine,
+		outpost, smithy, village, baron, great_hall };
+	int expected;
+	int actual;
+	int playerA = 0;
+	int playerB = 1;
+
+	initializeGame(numPlayers, k, seed, &control);
+
+	// copy the game state, test will actually be run while control will be manually modified to represent the expected values
+	test = control;
+
+	printf("Running tests for outpost card\n");
+
+	cardEffect(outpost, choice1, choice2, choice3, &test, handPos, &bonus);
+
+	//test hand count
+	printf("\nTest hand count for player A\n");
+	expected = control.handCount[playerA] - 1; //0 cards gained, 1 card played
+	actual = test.handCount[playerA];
+	printf("Expected: %d	Actual: %d\n", expected, actual);
+	isTrue(expected, actual);
+
+	//test deck count
+	printf("\nTest deck count for player A\n"); //deck remains unchanged
+	expected = control.deckCount[playerA];
+	actual = test.deckCount[playerA];
+	printf("Expected: %d	Actual: %d\n", expected, actual);
+	isTrue(expected, actual);
+
+	//test outpost flag
+	//expected to fail due to introduced bug
+	printf("\nTesting that outpost flag is set\n");
+	expected = control.outpostPlayed + 1;
+	actual = test.outpostPlayed;
+	printf("Expected: %d	Actual: %d\n", expected, actual);
+	isTrue(expected, actual);
+
+	//Tests to ensure victory cards are unchanged
+	printf("\nTesting that victory card piles remain unchanged\n");
+	printf("\nTesting province\n");
+	actual = test.supplyCount[province];
+	expected = control.supplyCount[province];
+	printf("Expected: %d	Actual: %d\n", expected, actual);
+	isTrue(expected, actual);
+
+	printf("\nTesting duchy\n");
+	actual = test.supplyCount[duchy];
+	expected = control.supplyCount[duchy];
+	printf("Expected: %d	Actual: %d\n", expected, actual);
+	isTrue(expected, actual);
+
+	printf("\nTesting estate\n");
+	actual = test.supplyCount[duchy];
+	expected = control.supplyCount[duchy];
+	printf("Expected: %d	Actual: %d\n", expected, actual);
+	isTrue(expected, actual);
+
+	//Tests to ensure kingdom cards are unchanged
+	printf("\nTesting that kingdom card piles remain unchanged\n");
+	for (int i = 0; i < 10; i++)
+	{
+		printf("\nTesting pile# %d\n", i);
+		actual = test.supplyCount[k[i]];
+		expected = control.supplyCount[k[i]];
+		printf("Expected: %d	Actual: %d\n", expected, actual);
+		isTrue(expected, actual);
+	}
+
+	//Tests to ensure other player is not affected
+	printf("\nTesting that there was no effect on playerB\n");
+	printf("\nTesting playerB's hand count\n");
+	actual = test.handCount[playerB];
+	expected = control.handCount[playerB];
+	printf("Expected: %d	Actual: %d\n", expected, actual);
+	isTrue(expected, actual);
+
+	printf("\nTesting playerB's deck count\n");
+	actual = test.deckCount[playerB];
+	expected = control.deckCount[playerB];
+	printf("Expected: %d	Actual: %d\n", expected, actual);
+	isTrue(expected, actual);
+
+	printf("\nTesting playerB's discard count\n");
+	actual = test.discardCount[playerB];
+	expected = control.discardCount[playerB];
+	printf("Expected: %d	Actual: %d\n", expected, actual);
+	isTrue(expected, actual);
+
+	if (failCount > 0)
+	{
+		printf("\nFINAL RESULT: TEST FAILED\n");
+		printf("Number of tests failed: %d\n", failCount);
+	}
+	else
+	{
+		printf("\nFINAL RESULT: ALL TESTS PASSED\n");
+	}
+
+
+	return 0;
+}
---
+++ projects/roccanoc/dominion/unittest2.c
@@ -0,0 +1,121 @@
+/*
+	Christian Roccanova
+	CS362-400
+	Assignment 3
+	tests updateCoins()
+*/
+
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include "rngs.h"
+
+// counter for total number of failures
+int failCount = 0;
+
+// checks if two values are equivalent
+void isTrue(int a, int b)
+{
+	if (a == b)
+	{
+		printf("TEST PASSED\n");
+	}
+	else
+	{
+		printf("TEST FAILED\n");
+		failCount++;
+	}
+}
+
+int main(int argc, char** argv) 
+{
+	struct gameState g;
+	int numPlayers = 2;
+	int seed = 1000;
+	int k[] = {adventurer, council_room, feast, gardens, mine
+		, remodel, smithy, village, baron, great_hall};
+	int bonus = 0;
+	int actual;
+	int expected;
+		
+	//fills arrays with respective treasure cards
+	for (int i = 0; i < MAX_HAND; i++) 
+	{
+		g.hand[0][i] = adventurer;
+	}
+
+	initializeGame(numPlayers, k, seed, &g);
+	g.handCount[0] = 5;
+
+	printf("Running tests for updateCoins() function\n");
+	
+	//test hand with 0 coin cards
+	printf("Test with 0 coins");	
+	for (int i = 0; i < 5; i++)
+	{
+		g.hand[0][i] = smithy;
+	}
+	updateCoins(0, &g, bonus);
+	expected = 0;
+	actual = g.coins;
+	printf("Expected: %d	Actual: %d\n", expected, actual);
+	isTrue(expected, actual);
+
+	//test with 5 coppers
+	printf("\nTesting with 5 coppers, no bonus - expect 5\n");
+	for (int i = 0; i < 5; i++)
+	{
+		g.hand[0][i] = copper;
+	}
+	updateCoins(0, &g, bonus);
+	actual = g.coins;
+	expected = 5;
+	printf("Expected: %d	Actual: %d\n", expected, actual);
+	isTrue(expected, actual);
+
+	//test with 5 silver
+	printf("\nTesting with 5 silvers, no bonus - expect 10\n");
+	for (int i = 0; i < 5; i++)
+	{
+		g.hand[0][i] = silver;
+	}
+	updateCoins(0, &g, bonus);
+	actual = g.coins;
+	expected = 10;
+	printf("Expected: %d	Actual: %d\n", expected, actual);
+	isTrue(expected, actual);
+
+	//test with 5 golds
+	printf("\nTesting with 5 golds, no bonus - expect 15\n");
+	for (int i = 0; i < 5; i++)
+	{
+		g.hand[0][i] = gold;
+	}
+	updateCoins(0, &g, bonus);
+	actual = g.coins;
+	expected = 15;
+	printf("Expected: %d	Actual: %d\n", expected, actual);
+	isTrue(expected, actual);
+
+	//test bonus
+	printf("\nTesting 5 golds, +5 bonus - expect 20\n");
+	bonus = 5;
+	updateCoins(0, &g, bonus);
+	actual = g.coins;
+	expected = 20;
+	printf("Expected: %d	Actual: %d\n", expected, actual);
+	isTrue(expected, actual);
+
+	if (failCount > 0)
+	{
+		printf("\nFINAL RESULT: TEST FAILED\n");
+		printf("Number of tests failed: %d\n", failCount);
+	}
+	else
+	{
+		printf("\nFINAL RESULT: ALL TESTS PASSED\n");
+	}
+
+	return 0;
+}
